f43060607176db60770e13162acefac56b4cd7c0
Winnie <asgh03226@gmail.com>
2022-12-14 16:11:04 +0800

    refactor day3 part 2

--- day3/example.js
+++ day3/example.js
@@ -1,26 +1,51 @@
 import data from './data'
 
-// setting sortList
+export default ()=>{
+  // part 1
+  let scoreList = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
-let scoreList = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const smaple = `vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw`
 
-const smaple = `vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`
+  let sum = 0;
 
-let sum = 0;
-
-const filterSentence = (d) => {
-  d.split('\n').map(i => {
-    let firstSentence = i.substring(0, )
+  data.split('\n').map(i=>{
+    let first = i.substring(0, [(i.length/2)]);
+    let second = i.substring([(i.length/2)], i.length);
+    let characters = [...first].filter(i => second.includes(i))[0];
+    sum += (scoreList.indexOf(characters)+1);
   })
+
+  console.log('part 1: ', sum);
+
+  // part 2
+
+  let sentence =`vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+  let list = data.split('\n')
+
+  const n = 3;
+
+  let sums = list.length/n; 
+  let total = 0 ;
+
+  for(let i = sums; i > 0; i--) {
+      let last =  ((i-1)*n)-(list.length) ;   
+      // 三個為一組
+      let item = list.slice((Math.abs(last)-n), Math.abs(last));
+      // 2比對1 找出一樣的字母
+      let char= [...item[2]].filter(i => item[0].includes(i) && item[1].includes(i))
+      total += scoreList.indexOf(char[0])+1
+  }
+
+  console.log('part 2: ' ,total);
 }
-data.split('\n').map(i=>{
-  let first = i.substring(0, [(i.length/2)]);
-  let second = i.substring([(i.length/2)], i.length);
-  let characters = [...first].filter(i => second.includes(i))[0];
-  sum += (scoreList.indexOf(characters)+1);
-})
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {  sumOfCals,  sumOfFirstThree } from '/day1/calorieCounting'
-import getScore  from '/day2/setGame'
-import data from './day3/data'
+import Day2  from '/day2/setGame'
+import Day3 from './day3/example'
 
  // day1
   // let findSumOfMax = Math.max(...sumOfCals());
@@ -8,65 +8,10 @@ import data from './day3/data'
 
   // console.log('total',total)
   // console.log('findSumOfMax', findSumOfMax)
+ 
+// Day2()
 
-// day2 
-// getScore()
-
-// day3 
-
-let scoreList = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-
-const smaple = `vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`
-
-let sum = 0;
-
-let a = data.split('\n').map(i=>{
-  let first = i.substring(0, (i.length/2));
-  let second = i.substring(i.length/2, i.length);
-  let characters = [...first].filter(i => second.includes(i))[0];
-  return scoreList.indexOf(characters)+1
-  // return characters
-})
-
-sum = a.reduce((a, c)=> a+c,0);
-
-// day3 part 2
-
-let sentence =`vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw`;
-
-let list = data.split('\n')
-
-const n = 3;
-
-let sums = list.length/n; 
-let total = 0 ;
-
-for(let i = sums; i > 0; i--) {
-    let last =  ((i-1)*n)-(list.length) ;   
-    // 三個為一組
-    let item = list.slice((Math.abs(last)-n), Math.abs(last));
-    // 跑每一個String 移除重複的字母
-    let a = item.map((i, idx) => {
-      if(idx !== 0){
-        let str = [...i].filter(i => item[0].includes(i));
-        return [...new Set(str)]
-      }
-    }).slice(1,3);   
-    let char = a[0].filter(i => a[1].includes(i));
-    total += scoreList.indexOf(char[0])+1    
-}
-
-
+Day3()
 
 
 
