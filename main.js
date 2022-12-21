f2b260fbf1bc85ed89ca0d7ca95f47fe1b9b174c
Winnie <asgh03226@gmail.com>
2022-12-21 10:34:36 +0800

    add day6 part 1

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ import Day2  from '/day2/setGame'
 import Day3 from './day3/example'
 import Day4 from './day4/example'
 import Day5 from './day5/example'
+import Day6 from './day6/example'
 
 // Day1();
 // Day2();
 // Day3();
 // Day4();
-// Day5.part2();
+// Day5;
+// Day6();
 
-// console.log(data);
 
-// day 5 part 1
+
 
 
 
